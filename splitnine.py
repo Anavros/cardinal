22279db713e40934adb9d91151a2199c566d9c0e
John Dobbs <jtdobbs1@me.com>
2016-08-09 04:25:33 -0400

    Image scaler is bascially functional.

--- splitnine.py
+++ splitnine.py
@@ -6,25 +6,29 @@ from vispy import io
 
 def stretch(path, new_path, x, y, d):
     image = io.imread(path)
+    #print(image.dtype, image.shape)
+    #input()
+    # image = 16, 16, 4
+    # newim = ((16-2d)*4)+2d == 28, 28, 4
 
-    # split the image into nine parts
-    pieces = {
-        "top": image[:d, d:-d, :],
-        "bot": image[-d:, d:-d, :],
-        "lft": image[d:-d, :d, :],
-        "rht": image[d:-d, -d:, :],
-        "mid": image[d:-d, d:-d, :],
-        "tlc": image[:d, :d, :],
-        "trc": image[:d, -d:, :],
-        "blc": image[-d:, :d, :],
-        "brc": image[-d:, -d:, :],
-    }
-    for k, v in pieces.items():
-        v = np.repeat(np.repeat(v, 4, axis=0), 4, axis=1)
-        io.imsave('pieces/{}.png'.format(k), v)
+    new_image = np.full((28, 28, 4), 256, dtype=np.uint8)
+    ##print(new_image.dtype, new_image.shape)
+    #input()
+    new_image[  : d,  d:-d, :] = _scale(image[  : d,  d:-d, :], 4, 1) # top
+    new_image[-d:  ,  d:-d, :] = _scale(image[-d:  ,  d:-d, :], 4, 1) # bot
+    new_image[ d:-d,   : d, :] = _scale(image[ d:-d,   : d, :], 1, 4) # lft
+    new_image[ d:-d, -d:  , :] = _scale(image[ d:-d, -d:  , :], 1, 4) # rht
+    new_image[ d:-d,  d:-d, :] = _scale(image[ d:-d,  d:-d, :], 4, 4) # mid
+    new_image[  : d,   : d, :] = image[  : d,   : d, :] # tlc
+    new_image[  : d, -d:  , :] = image[  : d, -d:  , :] # trc
+    new_image[-d:  ,   : d, :] = image[-d:  ,   : d, :] # blc
+    new_image[-d:  , -d:  , :] = image[-d:  , -d:  , :] # brc
 
-    new_image = np.repeat(np.repeat(image, 4, axis=0), 4, axis=1)
     io.imsave(new_path, new_image)
+
+
+def _scale(image, y, x):
+    return np.repeat(np.repeat(image, x, axis=0), y, axis=1)
 
 
 if __name__ == '__main__':
