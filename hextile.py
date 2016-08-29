3c690ebf69606a0c3d62a7b17a501b67a68dbd0b
John Dobbs <jtdobbs1@me.com>
2016-08-29 08:36:13 -0400

    Cannibalized my beau. 2D renderer for 3D.

--- hextile.py
+++ hextile.py
@@ -2,6 +2,7 @@
 
 from vispy import gloo, app, io
 from vispy.gloo import gl
+from vispy.util import transforms
 import numpy as np
 import random
 
@@ -14,6 +15,7 @@ SIZE, FLAT = 0.2, 1
 def main():
     app.use_app('glfw')
     gl.glEnable(gl.GL_BLEND)
+    gl.glEnable(gl.GL_DEPTH_TEST)
     gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
     canvas = AutoCanvas(
         title="Hexes", size=(W, H), keys="interactive", px_scale=Z)
@@ -25,7 +27,7 @@ def main():
     hex_index = gloo.IndexBuffer(gen_index())
     hex_texcoords = gen_texcoords()
 
-    hexmap = gen_map(COLS, ROWS)
+    hexmap, heightmap = gen_map(COLS, ROWS)
     #io.imsave('hexmap.png', hexmap)
 
     program['a_position'] = hex_vertex
@@ -38,11 +40,23 @@ def main():
     hover = [0, 0]
     program['u_camera'] = cam
 
+    # 3D Matrices
+    mats = CheatyStorage(
+        model=np.eye(4, dtype=np.float32),
+        view=np.eye(4, dtype=np.float32),
+        #perspective=transforms.ortho(0, 1, 0, 1, 0, 1),
+        perspective=transforms.perspective(90.0, 1.0, 0.5, 20.0),
+    )
+
+    mats.model = np.dot(mats.model, transforms.translate((0, 0, -10)))
+    program['m_model'] = mats.model
+    program['m_view'] = mats.view
+    program['m_perspective'] = mats.perspective
 
     @canvas.connect
     def on_draw(event):
         global printed
-        gloo.clear((1,1,1,1))
+        gloo.clear((1,1,1,1), 0.5)
         for c in range(COLS):
             for r in range(ROWS):
                 if hexmap[c, r] == 0:
@@ -50,42 +64,77 @@ def main():
                 else:
                     program['u_texture'] = grav_texture
                 x, y = (c*(3*SIZE/4), r*SIZE/FLAT + (SIZE/2/FLAT if (c%2==0) else 0))
-                program['u_offset'] = (x, y)
-                if [c, r] == hover:
-                    program['u_overlay'] = 0.1
-                else:
-                    program['u_overlay'] = 0.0
+                z = heightmap[c, r]
+
+                #program['u_offset'] = (x, y)
+                mats.model = transforms.translate((x, y, -10-z/10))
+                program['m_model'] = mats.model
+#                if [c, r] == hover:
+#                    program['u_overlay'] = 0.1
+#                else:
+#                    program['u_overlay'] = 0.0
                 program.draw('triangles', hex_index)
 
     @canvas.connect
     def on_mouse_move(event):
-        if not event.buttons:
+        # Middle-Click
+        if event.buttons == [3]:
+            (nx, ny) = event.pos
+            (ox, oy) = event.last_event.pos
+            (dx, dy) = (nx-ox, ny-oy)
+            wx = dx*10/W/Z
+            wy = dy*10/H/Z
+            mats.view = np.dot(mats.view, transforms.rotate(wy, [1,0,0]))
+            mats.view = np.dot(mats.view, transforms.rotate(wx, [0,1,0]))
+            program['m_view'] = mats.view
+        # Right-Click
+        if event.buttons == [2]:
+            (nx, ny) = event.pos
+            (ox, oy) = event.last_event.pos
+            (dx, dy) = (nx-ox, ny-oy)
+            wx = dx*10/W/Z
+            wy = dy*10/H/Z
+            mats.view = np.dot(mats.view, transforms.rotate(-wy*10, [1,0,0]))
+            mats.view = np.dot(mats.view, transforms.rotate(-wx*10, [0,1,0]))
+            mats.view = np.dot(mats.view, transforms.translate((-wx, wy, 0)))
+            program['m_view'] = mats.view
+        elif event.buttons == []:
             x, y = event.pos
             c, r = screen_to_index(x, y, cam)
             hover[0] = c
             hover[1] = r
-        else:
+        elif event.buttons == [1]:
             (nx, ny) = event.pos
             (ox, oy) = event.last_event.pos
             (dx, dy) = (nx-ox, ny-oy)
-            cam[0] += dx/W/Z*2/cam[2]
-            cam[1] += -dy/H/Z*2/cam[2]
-            program['u_camera'] = cam
+            #cam[0] += dx/W/Z*2/cam[2]
+            #cam[1] += -dy/H/Z*2/cam[2]
+            #program['u_camera'] = cam
+            wx = dx*10/W/Z
+            wy = dy*10/H/Z
+            mats.view = np.dot(mats.view, transforms.translate((wx, -wy, 0)))
+            program['m_view'] = mats.view
 
     @canvas.connect
     def on_mouse_wheel(event):
         #print(event.delta)
-        cam[2] += event.delta[1]/10
-        program['u_camera'] = cam
+        #cam[2] += event.delta[1]/10
+        #program['u_camera'] = cam
+        mats.view = np.dot(mats.view, transforms.translate((0, 0, event.delta[1])))
+        #mats.view = np.dot(mats.view, transforms.rotate(event.delta[1], [0,0,1]))
+        program['m_view'] = mats.view
 
     @canvas.connect
     def on_mouse_release(event):
         if len(event.trail()) <= 1:
-            x, y = event.pos
-            x, y = screen_to_world(x, y, cam)
-            c, r = world_to_index(x, y)
-            print("x:{:.2f} y:{:.2f}".format(x, y))
-            touch(hexmap, c, r)
+            mats.view = np.dot(mats.view, transforms.translate((+0.01, +0.01, -0.1)))
+            program['m_view'] = mats.view
+
+            #x, y = event.pos
+            #x, y = screen_to_world(x, y, cam)
+            #c, r = world_to_index(x, y)
+            #print("x:{:.2f} y:{:.2f}".format(x, y))
+            #touch(hexmap, c, r)
 
     canvas.start()
     app.run()
@@ -116,7 +165,9 @@ def touch(hexmap, c, r):
 
 
 def gen_map(cols, rows):
-    return np.random.randint(2, size=(rows, cols)).astype(np.uint32)
+    hexmap = np.random.randint(2, size=(rows, cols), dtype=np.uint32)
+    heightmap = np.random.randint(2, size=(rows, cols), dtype=np.uint32)
+    return hexmap, heightmap
 
 
 def gen_single(size, flat=1):
@@ -157,18 +208,25 @@ def gen_index():
     ]).astype(np.uint32)
 
 
+class CheatyStorage(object):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__dict__[k] = v
+
+
 VERTEX = r"""
 #version 120
 
 attribute vec2 a_position;
 attribute vec2 a_texcoord;
-uniform vec3 u_camera;
-uniform vec2 u_offset;
+uniform mat4 m_model;
+uniform mat4 m_view;
+uniform mat4 m_perspective;
 
 uniform float u_overlay;
 
 void main(void) {
-    gl_Position = vec4((a_position+u_camera.xy+u_offset.xy)*u_camera.z, 0.0, 1.0);
+    gl_Position = m_perspective * m_view * m_model * vec4(a_position, 0.0, 1.0);
     gl_TexCoord[0] = vec4(a_texcoord, 0.0, 0.0);
 }
 """
