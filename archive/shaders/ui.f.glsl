3d705e9b7e1a665cc0a3684cafad1322b9548f03
Anavros <jtdobbs1@me.com>
2016-10-31 09:33:22 -0400

    Removed old files.

--- archive/shaders/ui.f.glsl
+++
@@ -1,7 +0,0 @@
-#version 120
-
-uniform sampler2D t_gui;
-
-void main(void) {
-    gl_FragColor = texture2D(t_gui, gl_TexCoord[0].st);
-}
--- archive/shaders/ui.v.glsl
+++
@@ -1,9 +0,0 @@
-#version 120
-
-attribute vec2 a_ver;
-attribute vec2 a_tex;
-
-void main(void) {
-    gl_Position = vec4(a_ver, 0.0, 1.0);
-    gl_TexCoord[0] = vec4(a_tex, 0.0, 0.0);
-}
